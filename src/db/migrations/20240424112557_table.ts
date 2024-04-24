cf69e19df344622719b1b8d7c2411ac4ad828121
afyqeyzar <norafyqe@gmail.com>
2024-04-24 13:15:21 +0100

    feat: create initial migrations

--- src/db/migrations/20240424112557_table.ts
+++ src/db/migrations/20240424112557_table.ts
@@ -1,10 +1,14 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("image", (table) => {
+    table.string("id");
+    table.string("name");
+    table.string("description");
+    table.string("date");
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("image");
 }
-
