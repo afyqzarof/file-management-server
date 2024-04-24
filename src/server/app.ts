12cbfb107d855002a31f0e956ebf73bc339053c3
afyqeyzar <norafyqe@gmail.com>
2024-04-24 13:08:57 +0100

    feat: serve images from server

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,14 @@
 import express, { Application } from "express";
+import path from "path";
+import fileRoutes from "./routes/file.routes";
+
 const app: Application = express();
 
 app.route("/").get((req, res) => {
   res.json("hello");
 });
+
+app.use("/image", express.static(path.resolve(__dirname, "../../public")));
+app.use("/files", fileRoutes);
 
 export default app;
---
+++ src/server/routes/file.routes.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import fileController from "../controllers/file.controller";
+
+const router = express.Router();
+
+export default router;
