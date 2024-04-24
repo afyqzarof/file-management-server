1e7893d7237051bf8e59a2cc533791972e6710d0
afyqeyzar <norafyqe@gmail.com>
2024-04-24 13:27:49 +0100

    feat: add additional seeds

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # File Management Backend
+
+[Link to Frontend](https://github.com/afyqzarof/file-management-client)
+
+## To run
+
+1. Ensure .env file is filled in according to .env.sample
+2. Install dependencies:
+
+```bash
+npm install
+```
+
+3. Run migrations:
+
+```bash
+npm run migrate
+```
+
+4. Seed data:
+
+```bash
+npm run seed
+```
+
+5. Start development server:
+
+```bash
+npm run dev
+```
--- src/db/seed-data/image.ts
+++ src/db/seed-data/image.ts
@@ -6,4 +6,25 @@ export default [
     filename: "scribble.jpeg",
     date: "1713961313602",
   },
+  {
+    id: "2",
+    name: "fashion",
+    description: "fashion description",
+    filename: "demo.jpeg",
+    date: "1713961313702",
+  },
+  {
+    id: "3",
+    name: "music",
+    description: "music description",
+    filename: "demo2.jpeg",
+    date: "1713961315602",
+  },
+  {
+    id: "4",
+    name: "sunflower",
+    description: "sunflower description",
+    filename: "demo3.jpeg",
+    date: "1713961316602",
+  },
 ];
